ee45362520a1dda90156b404c64b887940ec0b64
NovikovIlya <91413531+NovikovIlya@users.noreply.github.com>
2024-03-21 12:44:56 +0000

    Добавил красоты

--- src/components/InputComponent/InputComponent.tsx
+++ src/components/InputComponent/InputComponent.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { useAppDispatch } from "../../hooks/redux";
+import { setData } from "../../store/todoSlice";
+import styles from "./InputComponent.module.css";
 
 const InputComponent = ({ setDataInfo }: any) => {
   const [text, setText] = useState("178.238.11.6");
+  const dispatch = useAppDispatch();
 
   const { refetch, data, isError, isLoading } = useQuery({
     queryKey: ["todos"],
@@ -15,6 +19,7 @@ const InputComponent = ({ setDataInfo }: any) => {
       );
       const data = await response.json();
       setDataInfo(data.location);
+      dispatch(setData(data));
       return data;
     } catch (error) {
       console.log(error);
@@ -30,12 +35,35 @@ const InputComponent = ({ setDataInfo }: any) => {
   };
 
   return (
-    <div>
-      <input onChange={handleInput} />
-      <button onClick={hanldeClick}>Submit</button>
+    <div className={styles.container}>
+      <div>IP adress tracker</div>
       <div>
-        <div>{data?.ip}</div>
+        <input placeholder="Enter IP adress" onChange={handleInput} />
+        <button onClick={hanldeClick}>Submit</button>
       </div>
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error</div>}
+      {data ? (
+        <>
+          <div className={styles.info}>
+            <div className={styles.border}>
+              <div>IP:</div> <div>{data?.ip}</div>
+            </div>
+            <div className={styles.border}>
+              <div>Location:</div>{" "}
+              <div>{`${data?.location.city}, ${data?.location.region}`}</div>
+            </div>
+            <div className={styles.border}>
+              <div>Timezone:</div> <div>{data?.location.timezone}</div>
+            </div>
+            <div>
+              <div>ISP:</div> <div>{data?.isp}</div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>No data...</>
+      )}
     </div>
   );
 };
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,37 +2,37 @@ import React, { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useAppSelector } from "../../hooks/redux";
 
-function Map({ dataInfo: { lat, lng } }) {
-  const [latValue, setLat] = useState();
-  const [lngValue, setLng] = useState();
+function Map() {
+  const { data } = useAppSelector((state) => state.todoSlice);
 
+  const [position, setposition] = useState([51.505, -0.09]);
   useEffect(() => {
-    setLat(lat);
-    setLng(lng);
-  }, [lng, lat]);
-  console.log(typeof lat, lng);
+    if (data) {
+      setposition([data.location.lat, data.location.lng]);
+    }
+  }, [data]);
 
   return (
     <>
-      {latValue && lngValue && (
-        <div className="ss">
-          <MapContainer
-            center={[latValue, lngValue]}
-            zoom={13}
-            scrollWheelZoom={false}
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[latValue, lngValue]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </MapContainer>
-        </div>
+      {data && (
+        <MapContainer
+          center={position}
+          zoom={13}
+          scrollWheelZoom={false}
+          key={position[0].toString()}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       )}
     </>
   );
